29ab1dcc48a4e38f6d13db588195cd2ad2cea758
Dharshik <dharshik06@gmail.com>
2026-03-17 22:35:58 +0530

    Add project files.

---
+++ TestPage.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Examist {
+    partial class TestPage {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.codeBox = new System.Windows.Forms.TextBox();
+            this.QuestionLabel = new System.Windows.Forms.Label();
+            this.verifyButton = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.timerLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // submitButton
+            // 
+            this.submitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.submitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitButton.Location = new System.Drawing.Point(801, 12);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(131, 49);
+            this.submitButton.TabIndex = 2;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.SubmitButton_Click);
+            // 
+            // codeBox
+            // 
+            this.codeBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.codeBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.codeBox.Location = new System.Drawing.Point(63, 85);
+            this.codeBox.Multiline = true;
+            this.codeBox.Name = "codeBox";
+            this.codeBox.Size = new System.Drawing.Size(818, 363);
+            this.codeBox.TabIndex = 3;
+            // 
+            // QuestionLabel
+            // 
+            this.QuestionLabel.AutoSize = true;
+            this.QuestionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.QuestionLabel.Location = new System.Drawing.Point(12, 25);
+            this.QuestionLabel.Name = "QuestionLabel";
+            this.QuestionLabel.Size = new System.Drawing.Size(188, 20);
+            this.QuestionLabel.TabIndex = 4;
+            this.QuestionLabel.Text = "Debug the following code";
+            // 
+            // verifyButton
+            // 
+            this.verifyButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.verifyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.verifyButton.Location = new System.Drawing.Point(664, 11);
+            this.verifyButton.Name = "verifyButton";
+            this.verifyButton.Size = new System.Drawing.Size(131, 49);
+            this.verifyButton.TabIndex = 5;
+            this.verifyButton.Text = "Verify";
+            this.verifyButton.UseVisualStyleBackColor = true;
+            this.verifyButton.Click += new System.EventHandler(this.VerifyButton_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // timerLabel
+            // 
+            this.timerLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.timerLabel.AutoSize = true;
+            this.timerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timerLabel.Location = new System.Drawing.Point(577, 23);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new System.Drawing.Size(81, 24);
+            this.timerLabel.TabIndex = 6;
+            this.timerLabel.Text = "⏲️ 30:00";
+            // 
+            // JavaTestPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 501);
+            this.Controls.Add(this.timerLabel);
+            this.Controls.Add(this.verifyButton);
+            this.Controls.Add(this.QuestionLabel);
+            this.Controls.Add(this.codeBox);
+            this.Controls.Add(this.submitButton);
+            this.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.Name = "JavaTestPage";
+            this.Text = "Examist";
+            this.Load += new System.EventHandler(this.TestPage_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.TextBox codeBox;
+        private System.Windows.Forms.Label QuestionLabel;
+        private System.Windows.Forms.Button verifyButton;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label timerLabel;
+    }
+}
+
---
+++ TestPage.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Examist {
+    struct Time {
+        int now;
+
+        public int Now {
+            get => now;
+            set => now = Math.Max(value, 0);
+        }
+
+        int Minutes => Now / 60;
+        int Seconds => Now % 60;
+
+        public Time(int timeInSeconds) {
+            now = timeInSeconds;
+        }
+
+        public bool IsEnded => Now <= 0;
+        public string NowAsString => $"{Minutes.PadZero()}:{Seconds.PadZero()}";
+    }
+
+    public partial class TestPage : Form {
+        bool canClose = false;
+        Time time = new Time(timeInSeconds: 30);
+
+
+        public TestPage() {
+            InitializeComponent();
+
+            timer.Start();
+            timer.Interval = 1000;
+
+            WindowState = FormWindowState.Maximized;
+            FormBorderStyle = FormBorderStyle.None;
+            TopMost = true;
+        }
+
+        private void TestPage_Load(object sender, EventArgs e) {
+            submitButton.Enabled = false;
+
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e) {
+            e.Cancel = !canClose;
+        }
+
+        void DisableTaskManager() {
+            RegistryKey key = Registry.CurrentUser.CreateSubKey(
+                @"Software\Microsoft\Windows\CurrentVersion\Policies\System");
+
+            key.SetValue("DisableTaskMgr", 1);
+        }
+
+        void KillPrograms() {
+            string[] blocked = {
+                "chrome",
+                "msedge",
+                "code",
+                "notepad"
+            };
+
+            foreach (string name in blocked) {
+                foreach (Process p in Process.GetProcessesByName(name)) {
+                    try {
+                        p.Kill();
+                    } catch { }
+                }
+            }
+        }
+
+        void EnableTaskManager() {
+            RegistryKey key = Registry.CurrentUser.CreateSubKey(
+                @"Software\Microsoft\Windows\CurrentVersion\Policies\System");
+
+            key.DeleteValue("DisableTaskMgr", false);
+        }
+
+        void SubmitButton_Click(object sender, EventArgs e) {
+            CloseApplication($"Test Completed in {time.NowAsString}");
+        }
+
+        void VerifyButton_Click(object sender, EventArgs e) {
+            string answer = codeBox.Text;
+
+            if (answer.Contains("boolean")) {
+                MessageBox.Show("No Errors Exists");
+                submitButton.SetActive(true);
+            } else {
+                MessageBox.Show("Error Exists");
+            }
+        }
+
+        void Timer_Tick(object sender, EventArgs e) {
+            time.Now--;
+
+            timerLabel.Text = $"⏲️:{time.NowAsString}";
+
+            if (time.IsEnded) {
+                CloseApplication("Test Time Over!");
+            }
+        }
+
+        void CloseApplication(string message) {
+            timer.Stop();
+            MessageBox.Show(message);
+            canClose = true;
+            Application.Exit();
+        }
+
+
+    }
+
+    public static class Extensions {
+        public static void SetActive(this Control control, bool active) {
+            control.Enabled = active;
+        }
+
+        public static string PadZero(this object text, int width = 2) {
+            return text.ToString().PadLeft(width, '0');
+        }
+    }
+}
